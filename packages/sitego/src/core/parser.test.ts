bcf96cccc7f1cbcc43803ede7a1182928e601c7c
nakita628 <takibilabdev@gmail.com>
2026-03-29 05:07:32 +0000

    add

--- packages/sitego/src/core/parser.test.ts
+++ packages/sitego/src/core/parser.test.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect } from 'vite-plus/test'
 
 import { getTextContent, getAttribute } from './dom.ts'
 import { parseHtml } from './parser.ts'
--- packages/sitego/src/core/rules.test.ts
+++ packages/sitego/src/core/rules.test.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect } from 'vite-plus/test'
 
 import { DEFAULT_OPTIONS } from './converter.ts'
 import { parseHtml } from './parser.ts'
